36164e4fa6640a5b0513a2957dfb5ec39402a67f
ZENat1319 <schopos.mail@gmail.com>
2020-05-13 16:09:20 +0900

    いろいろ

--- fk_sq.py
+++ fk_sq.py
@@ -1,4 +1,9 @@
 import sqlite3 as sq
+import os
+from datetime import datetime as dt
+
+Timetable = ["電子回路２", "応用数学１", "発変電工学", "英語", "第二外国語", "情報通信", "数値解析", "電子工学",
+             "日本経済論", "保健体育", "電気磁気学", "応用数学２", "回路網理論", "データベース論"]
 
 dbpath = 'kadais.sqlite'
 
@@ -7,17 +12,77 @@ cursor = connection.cursor()
 
 try:
     # CREATE
-    #cursor.execute("DROP TABLE IF EXISTS todo")
-    # dateの書式：yyyy-mm-dd hh:mm:ss
+    # なぜかなかったら新しく作る
+    if not os.path.isfile(dbpath):
+        print("データベースが存在しません。新たに作成します。")
     cursor.execute(
         "CREATE TABLE IF NOT EXISTS todo (class integer, date text, about text)")
 except sqlite3.Error as e:
     print('sqlite3.Error occurred:', e.args[0])
 
+# 日付のフォーマットをチェックする
+
+
+def check_format(sdate):
+    try:
+        fdate = dt.strptime(sdate, '%Y-%m-%d %H:%M:%S')
+        return True
+    except ValueError:
+        return False
+
 
 def todo_view():
+    print("\n現在の予定はこちらです")
     for row in cursor.execute("SELECT * FROM todo"):
         print(row)
+    print("")
+
+
+def todo_add():
+    print("\n課題を追加したい教科を選択してください(数字)")
+
+    for index, t in enumerate(Timetable):
+        print(str(index) + " " + t)
+    print("\n> ", end="")
+
+    ans_class = str(input())
+
+    while True:
+        print("提出期限を右の書式通りに入力してください(yyyy-mm-dd hh:mm:ss)> ", end="")
+        ans_date = str(input())
+        if check_format(ans_date):
+            break
+        else:
+            print("フォーマットエラー")
+
+    print("概要を入力してください> ", end="")
+    ans_about = str(input())
+
+    print("")
+    print("教科: " + Timetable[int(ans_class)])
+    print("日付: " + ans_date)
+    print("概要: " + ans_about)
+
+    while True:
+        print("これでよろしいですか？(Y/N/C)> ", end="")
+        chk = str(input())
+        if chk.lower() == "Y".lower():
+            # 追加する
+            try:
+                # INSERT
+                cursor.execute("INSERT INTO todo VALUES (?, ?, ?)",
+                               (ans_class, ans_date, ans_about))
+                print("追加しました\n")
+            except sq.Error as e:
+                print("追加に失敗しました("+e.args[0]+")\n")
+            break
+        elif chk.lower() == "N".lower():
+            # やり直し
+            return todo_add()
+        elif chk.lower() == "C".lower():
+            # キャンセル
+            print("\n")
+            return
 
 
 def end():
@@ -28,14 +93,13 @@ def end():
 
 while True:
 
-    print("課題管理プログラム\n1 課題予定追加, 2 課題予定参照, 3 通期機能ONOFF, 99 終了")
+    print("課題管理プログラム\n1 課題予定追加, 2 課題予定参照, 3 通期機能ONOFF, 99 終了> ", end="")
 
     answer = str(input())
     if answer == "1":
-        print("add")
+        todo_add()
 
     elif answer == "2":
-        print("view")
         todo_view()
 
     elif answer == "3":
