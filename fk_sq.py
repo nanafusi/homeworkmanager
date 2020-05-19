979a69819368ff6eae15d68d69c7bfba8786bea4
a <schopos.mail@gmail.com>
2020-05-20 00:52:18 +0900

    がんばった

--- fk_sq.py
+++ fk_sq.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime as dt
 import sys
 import tkinter as tk
+import tkinter.ttk as ttk
 import time
 
 Timetable = ["電子回路２", "応用数学１", "発変電工学", "英語", "第二外国語", "情報通信", "数値解析", "電子工学",
@@ -25,7 +26,7 @@ def initialize():
         # なぜかなかったら新しく作る
         # connectの段階でデータベースファイルは生成されるから注意
         cursor.execute(
-            "CREATE TABLE IF NOT EXISTS todo(class integer, date text, about text, key integer primary key)")
+            "CREATE TABLE IF NOT EXISTS todo(class text, date text, about text, key integer primary key)")
     except sqlite3.Error as e:
         print('sqlite3.Error occurred:', e.args[0])
 
@@ -33,7 +34,7 @@ def initialize():
 def check_format(sdate):
     # 日付のフォーマットをチェックする
     try:
-        fdate = dt.strptime(sdate, '%Y-%m-%d %H:%M:%S')
+        fdate = dt.strptime(sdate, '%Y-%m-%d %H:%M')
         return True
     except ValueError:
         return False
@@ -45,7 +46,7 @@ def todo_view():
     print("-"*60)
     for row in cursor.execute("SELECT * FROM todo ORDER BY date ASC"):
         print("{} {} {} {}".format(
-            row[3], Timetable[int(row[0])].ljust(5, "　"), row[1], row[2]))
+            str(row[3]).rjust(2,' '), row[0].ljust(5, "　"), row[1], row[2]))
     print("")
 
 
@@ -70,7 +71,7 @@ def todo_add():
     ans_class = str(input())
 
     while True:
-        print("提出期限を右の書式通りに入力してください(yyyy-mm-dd hh:mm:ss)> ", end="")
+        print("提出期限を右の書式通りに入力してください(yyyy-mm-dd hh:mm)> ", end="")
         ans_date = str(input())
         if check_format(ans_date):
             break
@@ -93,7 +94,7 @@ def todo_add():
             try:
                 # INSERT
                 cursor.execute("INSERT INTO todo VALUES (?, ?, ?, ?)",
-                               (ans_class, ans_date, ans_about, tmp_key))
+                               (Timetable[int(ans_class)], ans_date, ans_about, tmp_key))
                 # ここで連番振り直し
                 todo_order_key()
                 print("追加しました\n")
@@ -115,7 +116,7 @@ def todo_del():
     print("-"*60)
     for row in cursor.execute("SELECT * FROM todo ORDER BY date ASC"):
         print("{} {} {} {}".format(
-            row[3], Timetable[int(row[0])].ljust(5, "　"), row[1], row[2]))
+            row[3], row[0].ljust(5, "　"), row[1], row[2]))
     print("")
 
     print("> ", end="")
@@ -124,7 +125,7 @@ def todo_del():
     print("\n削除する予定はこれでよろしいですか")
     for row in cursor.execute("SELECT * FROM todo WHERE key = ?", (ans_id,)):
         print("{} {} {} {}".format(
-            row[3], Timetable[int(row[0])].ljust(5, "　"), row[1], row[2]))
+            row[3], row[0].ljust(5, "　"), row[1], row[2]))
 
 
 def end():
@@ -138,49 +139,75 @@ def changeFrame(frame):
     frame.tkraise()
 
 
+def reload_tree(tree):
+    i=0
+    tree.delete(*tree.get_children())
+    for r in cursor.execute("SELECT class,date,about FROM todo ORDER BY date ASC"):
+        tree.insert("","end",tags=i,values=r)
+        if i&1:
+            tree.tag_configure(i,background="#CCFFFF")
+        i+=1
+
+
 def form_loop():
     root = tk.Tk()
     root.title(u"Kadai kanri")
-    root.geometry("400x300")
+    root.geometry("333x300")
+    root.resizable(width=False, height=False)
 
     # メインフレームちそちそ
-    mainFrame = tk.Frame(root)
-    change_Btn = tk.Button(mainFrame, text="課題編集", command=lambda: todo_add())
-    change_Btn.pack()
+    f0 = tk.Frame(root)
+    f1=tk.Frame(root)
 
-    refer_Btn = tk.Button(mainFrame, text="課題参照", command=lambda: todo_view())
-    refer_Btn.pack()
+    # ツリービュー
+    kadai_tree=ttk.Treeview(root)
+    kadai_tree["columns"]=(1,2,3)
+    kadai_tree["show"]="headings"
+    kadai_tree.column(1,width=70)
+    kadai_tree.column(2,width=100)
+    kadai_tree.column(3,width=150)
+    kadai_tree.heading(1,text="教科")
+    kadai_tree.heading(2,text="期限")
+    kadai_tree.heading(3,text="概要")
 
-    exit_Btn = tk.Button(mainFrame, text="終了", command=lambda: end())
-    exit_Btn.pack()
+    reload_tree(kadai_tree)
 
-    mainFrame.grid(row=0, column=0, sticky="nsew")
+    kadai_tree.pack(in_=f1,fill=tk.BOTH)#grid(row=3,column=0,padx=5,pady=5,sticky=tk.W + tk.E)
 
-    # ちそちそフレーム
-    # tstsFrame = tk.Frame(root)
-    # tstsLabel = tk.Label(tstsFrame, text="ちそちそ")
-    # tstsLabel.pack()
-    # tstsFrame.grid(row=0, column=0, sticky="nsew")
+    # ボタンの配置
+    change_Btn = tk.Button(f0, text="課題追加", command=lambda: todo_add()).pack(side=tk.LEFT)
+    #change_Btn.grid(row=0,column=0,padx=5)
+    
+    refer_Btn = tk.Button(f0, text="課題更新", command=lambda: reload_tree(kadai_tree)).pack(side=tk.LEFT)
+    #refer_Btn.grid(row=0,column=1,padx=5)
 
-    mainFrame.tkraise()
+    delete_Btn = tk.Button(f0, text="課題削除", command=lambda: todo_del()).pack(side=tk.LEFT)
+    #delete_Btn.grid(row=0,column=2,padx=5)
+
+    exit_Btn = tk.Button(root, text="終了", command=lambda: end()).pack(in_=f1,fill=tk.BOTH)
+    #exit_Btn.grid(row=1,column=0,columnspan=3,padx=5,pady=5,sticky=tk.W + tk.E)
+
+    # フレームの配置(いらんと思う)
+    f0.pack()
+    f1.pack()
 
     root.mainloop()
 
-
+initialize()
+form_loop()
 while True:
-    initialize()
-    # form_loop()
-    print("課題管理プログラム\n1 課題予定追加, 2 課題予定参照, 3 課題予定削除, 99 終了> ", end="")
+    #print("課題管理プログラム\n1 課題予定追加, 2 課題予定参照, 3 課題予定削除, 99 終了> ", end="")
 
-    answer = str(input())
-    if answer == "1":
-        todo_add()
+    #answer = str(input())
+    #if answer == "1":
+    #    todo_add()
 
-    elif answer == "2":
-        todo_view()
+    #elif answer == "2":
+    #    todo_view()
 
-    elif answer == "3":
-        todo_del()
+    #elif answer == "3":
+    #    todo_del()
 
-    elif answer == "99":
-        end()
+    #elif answer == "99":
+    #    end()
+    pass
